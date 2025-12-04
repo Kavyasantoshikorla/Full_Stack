5c5c3c4e8f1628331dfb38e3bd3983c45e6e87dd
Kavyasantoshikorla <23p31a0531@acet.ac.in>
2025-12-04 10:31:39 +0530

    backend

---
+++ Backend/async.js
@@ -0,0 +1,17 @@
+function getUserDetails(id)
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{ 
+            resolve({userroll:"678"});
+        },2000);
+    })
+};
+const myfunc=async()=>{
+   console.log("123");
+   const result1 = await getUserDetails("123");
+   console.log(result1);
+   console.log("456");
+//    const result2 = await getUserDetails("167");
+//    console.log(result2);
+};
+myfunc()
