217a368c89b1488931a45ed210a7a10e34f0e8d2
Kavyasantoshikorla <23p31a0531@acet.ac.in>
2026-01-02 12:09:30 +0530

    created all four http methods

--- Backend/routers/studentRoutes.js
+++ Backend/routers/studentRoutes.js
@@ -1,8 +1,13 @@
 //importing the student controller
-import { getStudents,addStudent } from "../controllers/studentController.js";
+import { getStudents,addStudent, getStudentById ,addStudents,updateStudents,updateStudentsStatus, deleteAllStudents} from "../controllers/studentController.js";
 import express from "express";
 //creating the router
 const router = express.Router();
 router.get('/get-students',getStudents);
 router.post('/add-student',addStudent);
+router.post('/add-students',addStudents);
+router.get('/get-students-by-id/:userid',getStudentById);
+router.put('/update-students/:id',updateStudents);
+router.put("/update-students-status", updateStudentsStatus);
+router.delete("/delete-data",deleteAllStudents);
 export default router;
